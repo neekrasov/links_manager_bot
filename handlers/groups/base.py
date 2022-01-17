34e7caca60ba35a33099f12aeb8dcc2932dc4eef
DeNeMiX83 <daniil53891@yandex.ru>
2022-01-17 23:07:40 +0300

    add fix handler command_start in Groups

--- handlers/groups/base.py
+++ handlers/groups/base.py
@@ -10,28 +10,34 @@ from utils.db_api.models import Users, Groups
 
 
 def register_user(user_id, full_name):
+    user = session.query(Users).get(user_id)
+    if user:
+        return False
     user = Users(chat_id=user_id,
                  full_name=full_name)
     session.add(user)
     session.commit()
+    return True
 
 
 def register_groups(message: types.Message):
+    group = session.query(Groups).get()
+    if group:
+        return False
     groups = Groups(chat_id=message.chat.id,
                     user_id=message.from_user.id,
                     name=message.chat.title)
     session.add(groups)
     session.commit()
+    return True
 
 
 @dp.message_handler(IsGroup(), IsGroupAdmin(), CommandStart())
 async def command_start(message: types.Message):
     user_id = message.from_user.id
     full_name = message.from_user.full_name
-    user = session.query(Users).get(user_id)
-    if not user:
-        register_user(user_id, full_name)
-        register_groups(message)
+    register_user(user_id, full_name)
+    register_groups(message)
     await message.answer(f"Привет, {full_name}")
 
 
