37f169e97005406c399f08b67eacbb52eafa0853
DeNeMiX83 <daniil53891@yandex.ru>
2022-03-02 12:09:38 +0300

    fix update date in mailing

--- tg_bot/utils/user_mailing.py
+++ tg_bot/utils/user_mailing.py
@@ -22,11 +22,13 @@ async def update_date_for_link(task: dict):
     await update_for_link(task_id, date=new_date)
 
 
-async def mailing(link_id: int, chat_id: int):
+async def mailing(task: dict, link_id: int, chat_id: int):
     await answer_link(link_id, chat_id)
+    await scheduler_update_date_for_link(task)
 
 
-async def scheduler_update_date_for_link(task: dict, task_datetime_finish: datetime):
+async def scheduler_update_date_for_link(task: dict):
+    task_datetime_finish = datetime.combine(task["date"], task["time_finish"])
     scheduler.add_job(update_date_for_link,
                       trigger='date',
                       next_run_time=task_datetime_finish,
@@ -39,7 +41,6 @@ async def scheduler_add_job(task: dict):
     # получаем время начала мероприятия
     task = get_datetime_from_str(task)
     task_datetime_start = datetime.combine(task["date"], task["time_start"]) - prearranged_link_minutes
-    task_datetime_finish = datetime.combine(task["date"], task["time_finish"])
 
 
     link = await get_link(task['link_id'])
@@ -56,22 +57,21 @@ async def scheduler_add_job(task: dict):
         else:
             task_datetime_start += timedelta(days=task["repeat"]) * (different_time.days // task["repeat"])
             task['date'] = task_datetime_start.date()
-            await scheduler_update_date_for_link(task, task_datetime_finish)
+            await scheduler_update_date_for_link(task)
 
     logger.debug(f"Задание link_id({task['link_id']}) запустится в {task_datetime_start}")
 
     add_job_kwargs = {
         'trigger': "date",
         'next_run_time': task_datetime_start,
-        'args': (link['id'], link['group_id'])
+        'args': (task, link['id'], link['group_id'])
     }
     if task["repeat"] != 0:
         add_job_kwargs['trigger'] = 'interval'
         add_job_kwargs['seconds'] = int(task["repeat"] * 60 * 60 * 24)
     scheduler.add_job(mailing,
                       **add_job_kwargs)
-    if task["repeat"] != 0:
-        await scheduler_update_date_for_link(task, task_datetime_finish)
+
 
 
 async def start_mailing():
